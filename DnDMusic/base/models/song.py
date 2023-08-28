d624857200d3cc2cc07e34d613533f2c75d7a7dc
ashbo <anneb97@gmx.de>
2023-08-28 22:50:53 +0200

    Implement tagging of songs

--- DnDMusic/base/models/song.py
+++ DnDMusic/base/models/song.py
@@ -1,1 +1,19 @@
 from django.db import models
+from tagging import Description
+
+class Song:
+    def __init__(self) -> None:
+        self.descriptions = []
+
+    def give_tag(self, description:Description):
+        if self.__is_duplicate_tag__(description):
+            self.__remove_same_tags__(description)
+        self.descriptions.append(description)
+
+    def __is_duplicate_tag__(self, description:Description) -> bool:
+        tags = {description.get_tag() for description in self.descriptions}
+        tag = description.get_tag()
+        return tag in tags
+    
+    def __remove_same_tags__(self, description:Description):
+        self.descriptions = [item for item in self.descriptions if item.get_tag() != description.get_tag()]
--- DnDMusic/base/models/tagging.py
+++ DnDMusic/base/models/tagging.py
@@ -1,12 +1,12 @@
 from base.exceptions import TaggingException
 
-class Description:
-    def __init__(self, tag, value) -> None:
-        self.tag = tag
-        if tag.is_valid_value(value):
-            self.value = value
-        else: raise TaggingException("Incorrect value for this type of tag")
+class Scale:
+    def __init__(self, name, values) -> None:
+        self.name = name
+        self.values = values
 
+    def is_valid_value(self, value):
+        return value in self.values
 
 class Tag:
     def __init__(self, name, scale) -> None:
@@ -18,13 +18,13 @@ class Tag:
             return False
         return self.scale.is_valid_value(value)
 
+class Description:
+    def __init__(self, tag, value) -> None:
+        self.tag = tag
+        if tag.is_valid_value(value):
+            self.value = value
+        else: raise TaggingException("Incorrect value for this type of tag")
 
-class Scale:
-    def __init__(self, name, values) -> None:
-        self.name = name
-        self.values = values
-
-    def is_valid_value(self, value):
-        return value in self.values
-
+    def get_tag(self) -> Tag:
+        return self.tag
 
