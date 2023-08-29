df708d198e3505397154be4dd405a5c904966f45
oegge <oeggge@gmail.com>
2023-08-29 22:20:34 +0200

    delete useless files

--- src/dndmusic/control/urls.py
+++ src/dndmusic/control/urls.py
@@ -1,7 +1,6 @@
 
 
-from django.urls import include, path
-from dndmusic.control.views.home import MainPage
+from django.urls import path
 from dndmusic.control.views import home
 urlpatterns = [
     path('home', home.MainPage.as_view(),name='home')
