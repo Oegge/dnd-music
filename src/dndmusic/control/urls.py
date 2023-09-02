36fc8e2a4150666964e5eb17c92ba5c7f361d851
oegge <oeggge@gmail.com>
2023-09-02 19:23:06 +0200

    main html

--- src/dndmusic/control/urls.py
+++ src/dndmusic/control/urls.py
@@ -3,5 +3,6 @@
 from django.urls import path
 from dndmusic.control.views import home
 urlpatterns = [
-    path('home', home.MainPage.as_view(),name='home')
+    path('home', home.MainPage.as_view(),name='home'),
+    path('', home.LandingPage.as_view(),name='')
 ]
--- src/dndmusic/control/views/home.py
+++ src/dndmusic/control/views/home.py
@@ -1,19 +1,21 @@
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse
 
 from django.views.generic import ListView
 from dndmusic.base.models.song import Song
 from django.shortcuts import render, redirect
 
-
-class MainPage(View):
-    template_name='control/home.html'
-    def get(self, request):
-        return render(request,self.template_name,context={})
+class DnDView(View,LoginRequiredMixin):
     pass
 
-class ListSongs(ListView,LoginRequiredMixin,):
-    model=Song
-    template_name='test.html'
-    context_object_name='Songs'
-    paginate_by=10
+class LandingPage(DnDView):
+    def get(self, request):
+        return redirect(reverse('control:home'))
+
+class MainPage(DnDView):
+    
+    template_name='control/home.html'
+    
+    def get(self, request):
+        return render(request,self.template_name,context={})
