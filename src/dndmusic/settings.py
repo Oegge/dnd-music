b475ab150c86fe44659cb592a1dc40ec28141336
oegge <oeggge@gmail.com>
2023-09-02 18:57:41 +0200

    setup background images and main.html

--- src/dndmusic/settings.py
+++ src/dndmusic/settings.py
@@ -18,6 +18,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"  
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
